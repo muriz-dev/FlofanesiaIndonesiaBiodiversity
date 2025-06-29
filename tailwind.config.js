b08bb2fce2a61ee24be6cca79ff549d9485c18d2
muriz-dev <mhmdrizki.dev@gmail.com>
2025-06-29 20:53:38 +0700

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {
+      // Di sini Anda bisa menambahkan kustomisasi warna, font, dll.
+    },
+  },
+  plugins: [],
+}
